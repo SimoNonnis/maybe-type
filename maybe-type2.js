3a326dd4762d090f2953323d5c7a224ef25f315b
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-10 11:08:23 +0100

    :art: Use safe and predicates from crocks.

--- maybe-type2.js
+++ maybe-type2.js
@@ -1,16 +1,21 @@
 const Maybe = require('crocks/Maybe');
+const safe = require('crocks/Maybe/safe');
+const isNumber = require('crocks/predicates/isNumber');
+const isString = require('crocks/predicates/isString');
 
 const inc = n => n + 1;
 const toUpper = s => s.toUpperCase();
 
-const isNumber = n => typeof n === 'number';
-const isString = s => typeof s === 'string';
-const safe = (val, pred) => (pred(val) ? Maybe.Just(val) : Maybe.Nothing());
+// const isNumber = n => typeof n === 'number';
+// const isString = s => typeof s === 'string';
+// const safe = (pred, val) => (pred(val) ? Maybe.Just(val) : Maybe.Nothing());
+const safeN = safe(isNumber);
+const safeS = safe(isString);
 
-const inputN = safe(10, isNumber);
+const inputN = safeN(10);
 const resultN = inputN.map(inc);
 console.log(resultN);
 
-const inputS = safe('hello', isString);
+const inputS = safeS('Hello world');
 const resultS = inputS.map(toUpper);
 console.log(resultS);
