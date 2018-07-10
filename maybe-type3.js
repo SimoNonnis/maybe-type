25163857ce93b55f3f8a8d04651c9e9d626f1069
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-10 14:27:26 +0100

    :fire: Remove require Maybe.

---
+++ maybe-type3.js
@@ -0,0 +1,20 @@
+const safe = require('crocks/Maybe/safe');
+const isNumber = require('crocks/predicates/isNumber');
+const isString = require('crocks/predicates/isString');
+
+const inc = n => n + 1;
+const toUpper = s => s.toUpperCase();
+
+// const isNumber = n => typeof n === 'number';
+// const isString = s => typeof s === 'string';
+// const safe = (pred, val) => (pred(val) ? Maybe.Just(val) : Maybe.Nothing());
+const safeN = safe(isNumber);
+const safeS = safe(isString);
+
+const inputN = safeN(10);
+const resultN = inputN.map(inc);
+console.log(resultN);
+
+const inputS = safeS('Hello world');
+const resultS = inputS.map(toUpper);
+console.log(resultS);
