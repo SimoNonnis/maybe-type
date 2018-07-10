8548eca3fb81f87fae8b834bc567120080802bea
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-10 10:58:24 +0100

    :recycle: Refactor

--- maybe-type2.js
+++ maybe-type2.js
@@ -3,15 +3,14 @@ const Maybe = require('crocks/Maybe');
 const inc = n => n + 1;
 const toUpper = s => s.toUpperCase();
 
-const safeNum = val =>
-  typeof val === 'number' ? Maybe.Just(val) : Maybe.Nothing();
-const safeString = val =>
-  typeof val === 'string' ? Maybe.Just(val) : Maybe.Nothing();
+const isNumber = n => typeof n === 'number';
+const isString = s => typeof s === 'string';
+const safe = (val, pred) => (pred(val) ? Maybe.Just(val) : Maybe.Nothing());
 
-const inputN = safeNum(10);
+const inputN = safe(10, isNumber);
 const resultN = inputN.map(inc);
 console.log(resultN);
 
-const inputS = safeString('hello');
+const inputS = safe('hello', isString);
 const resultS = inputS.map(toUpper);
 console.log(resultS);
