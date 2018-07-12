362a102ddf3b0b04d88309843c86e05b534288ae
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-12 11:09:31 +0100

    :recycle: Use compose

--- maybe-type3.js
+++ maybe-type3.js
@@ -1,15 +1,17 @@
 const safe = require('crocks/Maybe/safe');
 const isNumber = require('crocks/predicates/isNumber');
+const compose = require('crocks/helpers/compose');
 
 const inc = n => n + 1;
 const dbl = n => n * 2;
-const incDbl = n => {
-  const safeN = safe(isNumber, n);
-  return safeN
-    .map(inc)
-    .map(dbl)
+const incDbl = n =>
+  safe(isNumber, n)
+    .map(
+      compose(
+        dbl,
+        inc
+      )
+    )
     .option(0);
-};
-
 const result = incDbl(9);
 console.log(result);
