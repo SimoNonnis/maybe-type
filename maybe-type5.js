89655d7c40fbf36b819b0aae3385d1f2fa40c2a5
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-12 11:46:34 +0100

    :recycle: Improve using compose, not, isNil from Ramda

--- maybe-type5.js
+++ maybe-type5.js
@@ -1,12 +1,14 @@
 const safe = require('crocks/Maybe/safe');
-const isNumber = require('crocks/predicates/isNumber');
-const compose = require('crocks/helpers/compose');
+const { compose, isNil, not } = require('ramda');
 
 const inc = n => n + 1;
 const query = { currentPage: 2, pageSize: 10, totalPage: 203 };
-const { currentPage } = query;
-const nextPage = safe(isNumber, currentPage)
-  .map(inc)
-  .option(0);
+
+const isNotNil = compose(
+  not,
+  isNil
+);
+const currentPage = safe(isNotNil, query.currentPage);
+const nextPage = currentPage.map(inc).option(1);
 
 console.log(nextPage);
