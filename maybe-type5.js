676f9bbbbe1a2c236d4b32152967eb0cd9da8b15
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-12 12:34:45 +0100

    :art: Use chain

--- maybe-type5.js
+++ maybe-type5.js
@@ -1,14 +1,13 @@
+const prop = require('crocks/Maybe/prop');
+const chain = require('crocks/pointfree/chain');
 const safe = require('crocks/Maybe/safe');
-const { compose, isNil, not } = require('ramda');
+const isNumber = require('crocks/predicates/isNumber');
 
 const inc = n => n + 1;
-const query = { currentPage: 2, pageSize: 10, totalPage: 203 };
+const query = { currentPage: 4, pageSize: 10, totalPage: 203 };
 
-const isNotNil = compose(
-  not,
-  isNil
-);
-const currentPage = safe(isNotNil, query.currentPage);
+const page = prop('currentPage', query);
+const currentPage = page.chain(safe(isNumber));
 const nextPage = currentPage.map(inc).option(1);
 
 console.log(nextPage);
