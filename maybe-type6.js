bb0371805fc2b88a5008de29ccc11496dedbe37f
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-13 09:40:24 +0100

    :sparkles: Get value from nested objs with propPath

---
+++ maybe-type6.js
@@ -0,0 +1,18 @@
+const propPath = require('crocks/Maybe/propPath');
+
+const user = {
+  username: 'tester',
+  email: 'test@gmail.com',
+  address: {
+    street: '111 E. West St',
+    city: 'Anywhere',
+    state: 'PA',
+    postalCode: '19123-4567'
+  }
+};
+
+const getPostalCode = propPath(['address', 'postalCode']);
+const zip = getPostalCode(user).option('not available');
+// const zip = propPath(['address', 'postalCode'], user).option('not available');
+
+console.log(zip);
