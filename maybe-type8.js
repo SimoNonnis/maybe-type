5c2132bd6fcdf503e8c80bdff86686d9cd2b9a9c
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-16 12:04:19 +0100

    :sparkles: Use alt

--- maybe-type8.js
+++ maybe-type8.js
@@ -1,12 +1,23 @@
 const crocks = require('crocks');
-const { and, compose, isEmpty, isString, Maybe, not, prop, safe } = crocks;
+const {
+  and,
+  compose,
+  isEmpty,
+  isString,
+  Maybe,
+  not,
+  prop,
+  safe,
+  chain
+} = crocks;
 const { join, split, toLower } = require('ramda');
 
 const article = {
   id: 1,
-  name: 'evilsoft/crocks'
+  name:
+    'evilsoft/crocks A collection of well known Algebraic Data Types for your utter enjoyment'
 };
-const url = 'https://github.com/';
+const gitHubUrl = 'https://github.com/';
 
 const isNotEmptyString = and(not(isEmpty), isString);
 const makeUrlSlug = compose(
@@ -15,3 +26,17 @@ const makeUrlSlug = compose(
   toLower
 );
 const makeUrl = slug => `https://github.com/${slug}`;
+const createUrlComposed = compose(
+  makeUrl,
+  makeUrlSlug
+);
+const getArticleName = obj =>
+  prop('name', obj)
+    .chain(safe(isNotEmptyString))
+    .alt(Maybe.of('404'));
+
+const url = getArticleName(article)
+  .map(createUrlComposed)
+  .option('');
+
+console.log(url);
