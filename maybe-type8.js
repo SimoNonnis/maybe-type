fbe3bd103a4622ce049cbfaebc14d1da67df8178
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-16 07:58:23 +0100

    :tada: Recover from Nothing

--- maybe-type8.js
+++ maybe-type8.js
@@ -6,3 +6,12 @@ const article = {
   id: 1,
   name: 'evilsoft/crocks'
 };
+const url = 'https://github.com/';
+
+const isNotEmptyString = and(not(isEmpty), isString);
+const makeUrlSlug = compose(
+  join('-'),
+  split(' '),
+  toLower
+);
+const makeUrl = slug => `https://github.com/${slug}`;
